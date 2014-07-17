4ae4d00d9005ebbbb5f5211d2b3e55108d2c1280
Mark Laane <marklaane@gmail.com>
2014-07-17 09:58:53 +0300

    removed "colors" and added comments

--- LED_matrix_15x8_TLC5940.ino
+++ LED_matrix_15x8_TLC5940.ino
@@ -6,22 +6,10 @@ int8_t width = 15;
 int8_t height = 8;
 TLC5940_panel panel(width, height);
 
-// Color definitions
-#define BLACK    0x0000
-#define BLUE     0x001F
-#define RED      0xF800
-#define GREEN    0x07E0
-#define CYAN     0x07FF
-#define MAGENTA  0xF81F
-#define YELLOW   0xFFE0 
-#define WHITE    0xFFFF
-
 void setup()
 {
-
-	panel.fillScreen(BLUE);
-	panel.drawCircle(3, 3, 2, GREEN);
-	
+	panel.fillTriangle(10, 0, 14, 4, 8, 7, 1000);
+	panel.drawCircle(3, 3, 2, 4095); //4095 is maximum value
 }
 
 void loop()
--- TLC5940_panel.cpp
+++ TLC5940_panel.cpp
@@ -4,7 +4,7 @@
 
 
 TLC5940_panel::TLC5940_panel(uint8_t width, uint8_t height) : Adafruit_GFX(width, height) {
-	int buffsize = 2 * 15 * 8;
+	int buffsize = 2 * width * height; // 2 bytes per pixel
 	if (NULL == (matrixbuff = (uint16_t *)malloc(buffsize))) return;
 	memset(matrixbuff, 0, buffsize);
 
@@ -17,16 +17,25 @@ void TLC5940_panel::update_next_col(){
 	display_col(curr_col);
 }
 void TLC5940_panel::drawPixel(int16_t x, int16_t y, uint16_t color){
-	matrixbuff[x + y * WIDTH] = color;
+	if (x >= WIDTH) return;
+	if (y >= HEIGHT) return;
+	matrixbuff[y * WIDTH + x] = color;
 }
 
 void TLC5940_panel::display_col(uint8_t col){
+	//Turn off all LED's
 	Tlc.clear();
 	Tlc.update();
+	//choose column mosfet
 	PORTC = (1 << col);
+	//Set up new data
+	//This could be optimised - 
+	// Tlc5940 library could be modified so there would be array of tlc_GSData buffers
+	// then drawpixel would use Tlc.set and here we only would select right buffer to display.
 	for (uint8_t row = 0; row < HEIGHT; row++){
 		Tlc.set(row, matrixbuff[row * WIDTH + col]);
 	}
+	//Push data out
 	Tlc.update();
 }
 
