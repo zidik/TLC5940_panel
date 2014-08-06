0e1970dd0ef96bcbc651fa24c900d274d11bc8b6
Mark Laane <marklaane@gmail.com>
2014-08-06 12:12:45 +0300

    now using updated pinmapping

--- TLC5940_panel.cpp
+++ TLC5940_panel.cpp
@@ -4,7 +4,9 @@
 
 
 TLC5940_panel::TLC5940_panel(uint8_t width, uint8_t height) : Adafruit_GFX(width, height) {
-	DDRC = 0xFF; //Port C outputs
+	DDRC |= B00111111;   // Port C (Analog 0 - Analog 5)
+	DDRD |= (1 << PD7);  // Digital 7
+	DDRB |= (1 << PB0);  // Digital 8
 }
 
 void TLC5940_panel::update_next_row(){
@@ -19,13 +21,21 @@ void TLC5940_panel::drawPixel(int16_t x, int16_t y, uint16_t brightness){
 }
 
 void TLC5940_panel::display_row(uint8_t row){
-	//Turn off mosfets
-	PORTC = 0xFF;
+	//Turn off mosfets by setting all outputs high
+	PORTC |= B00111111;   // Port C (Analog 0 - Analog 5)
+	PORTD |= (1 << PD7);  // Digital 7
+	PORTB |= (1 << PB0);  // Digital 8
 	//Display the right buffer
 	Tlc.setActiveBuffer(row);
 	Tlc.update();
-	//choose column mosfet
-	PORTC = ~(1 << row);
+	//choose column mosfet - set it low
+	if (row >= 0 && row < 6)
+		PORTC &= ~(1 << row);
+	else if (row == 6)
+		PORTD &= ~(1 << PD7);
+	else if (row == 7)
+		PORTB &= ~(1 << PB0);
+	else{}
 }
 
 
